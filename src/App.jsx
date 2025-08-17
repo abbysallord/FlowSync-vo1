1222dc07082662457da561d3e3d16d83e37fb407
Dhanush Shenoy H <dshenoyh@gmail.com>
2025-08-17 13:00:19 +0530

    Initial react setup complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Header from "./components/Header/Header"
+
+export default function App(){
+  return(
+    <div className="App">
+      <Header />
+      <main className="main-content">
+        <p>Task Management features coming soon...</p>
+      </main>
+    </div>
+  )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css"
+
+export default function Header(){
+    return(
+        <header className="header">
+            <h1>Task Master</h1>
+            <p>Organize your tasks like a react pro!</p>
+        </header>
+    )
+} 
