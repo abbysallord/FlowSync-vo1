0f693fb532fdf57ee57ef5f804edd320d9d689c3
Dhanush Shenoy H <dshenoyh@gmail.com>
2025-08-17 20:02:19 +0530

    fixed some bugs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css"
 function Header(){
     return(
         <header className="header">
-            <h1>Task Master</h1>
+            <h1>FlowSync</h1>
             <p>Organize your tasks like a react pro!</p>
         </header>
     )
