e430fcf32fe78686b03c525d62ab3d4434f037f3
Dhanush Shenoy H <dshenoyh@gmail.com>
2025-08-18 04:26:09 +0530

    A minor heare component bug fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ function Header(){
     return(
         <header className="header">
             <h1>FlowSync</h1>
-            <p>Organize your tasks like a react pro!</p>
+            <p>Organize your tasks like a pro!</p>
         </header>
     )
 } 
